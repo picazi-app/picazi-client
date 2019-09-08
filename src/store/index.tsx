3467ec524731485fda8eb9167bcc022eba349740
pooyax14 <pooyax14@gmail.com>
2019-09-08 21:18:25 +0530

    remove history from middleware

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -4,13 +4,14 @@ import { defaultRegistrationFormProps } from "../screens/Register/store/reducer"
 import { defaultPostListScreenProps } from "../screens/PostList/store/reducer";
 import { defaultPostInfoScreenProps } from "../screens/Post/store/reducer";
 import { defaultUserSessionProps } from './reducers';
+import { history } from "../helpers/history";
 
-import {createBrowserHistory} from 'history'
 import rootReducer from './reducers';
 // react-router has its own Redux middleware, so we'll use this
 import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import { StateProps } from './types'
+import { createBrowserHistory } from 'history';
 
 
 export function configureDefaultState() {
@@ -26,15 +27,13 @@ export function configureDefaultState() {
 }
 
 export function configureStore(defaultState: StateProps) {
-    // Create a history of your choosing (we're using a browser history in this case)
-  const history1 = createBrowserHistory()
-
-  // Build the middleware for intercepting and dispatching navigation actions
-  const middleware = routerMiddleware(history1)
+  
+  // const history = createBrowserHistory();
+  // const middleware = routerMiddleware(history)
 
   const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
   // const store = createStore(rootReducer, defaultState, enhancers);
-  const store = createStore(rootReducer, defaultState, composeEnhancers(applyMiddleware(middleware, thunk)));
+  const store = createStore(rootReducer, defaultState, composeEnhancers(applyMiddleware(thunk)));
   return store;
 }
